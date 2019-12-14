1211391646c3cdc37316ef7b79dee42a3b44a3e9
rduvql <26932928+rduvql@users.noreply.github.com>
2019-12-14 15:38:33 +0100

    can now select next / previous errors in documents

--- src/extension.ts
+++ src/extension.ts
@@ -1,66 +1,20 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { jumperCommands } from "./commands";
 
-// vscode.window.showInformationMessage('Hello World!');
+const STATE_PREVIOUS_VERSION_NUM = "STATE_PREVIOUS_VERSION";
 
-export function activate(context: vscode.ExtensionContext)
+export function activate( context: vscode.ExtensionContext )
 {
-    let disposable_previous = vscode.commands.registerCommand("ext.jumper.previous", () => {
-        let line = getNextOrPreviousLine(false);
+    jumperCommands.registerCommands( context );
 
-        line && moveToLine(line);
-    });
-    context.subscriptions.push(disposable_previous);
+    let jumper = vscode.extensions.getExtension( "rduvql.vscode-jumper" );
 
-	let disposable_next = vscode.commands.registerCommand("ext.jumper.next", () => {
+    let currentVersion = parseInt( ( jumper.packageJSON.version as string ).replace( /\./g, "" ) );
+    // let previousVersion = context.workspaceState.get<number>( STATE_PREVIOUS_VERSION_NUM );
 
-        let line = getNextOrPreviousLine(true);
 
-        line && moveToLine(line);
-    });
-	context.subscriptions.push(disposable_next);
+
+    context.workspaceState.update( STATE_PREVIOUS_VERSION_NUM, currentVersion );
 }
 
-export function deactivate() {}
-
-function getNextOrPreviousLine(isNext: boolean): vscode.TextLine | undefined
-{
-    const editor = vscode.window.activeTextEditor;
-
-    if(editor)
-    {
-        let currLine = editor.selection.active.line;
-
-        // dynamic for-loop that change direction base on 'isNext':
-        // isNext === true => loop forward else loop backward
-        // kind of ugly but nice at the same time
-        for (let i = (isNext ? currLine + 1 : currLine - 1); isNext ? i < editor.document.lineCount : i >= 0; isNext ? i++ : i--)
-        {
-            let line = editor.document.lineAt(i);
-
-            if(!line.isEmptyOrWhitespace && /^[a-zA-Z]{2,}/.test(line.text.trim())) {
-                return line;
-            }
-        }
-    }
-
-    return undefined;
-}
-
-function moveToLine(nextline: vscode.TextLine): void
-{
-    const editor = vscode.window.activeTextEditor;
-
-    if(editor)
-    {
-        editor.selection = new vscode.Selection(
-            new vscode.Position(nextline.lineNumber, nextline.firstNonWhitespaceCharacterIndex),
-            new vscode.Position(nextline.lineNumber, nextline.firstNonWhitespaceCharacterIndex)
-        );
-
-        // scroll when moving out of current range
-        editor.revealRange(new vscode.Range(
-            new vscode.Position(nextline.lineNumber, nextline.firstNonWhitespaceCharacterIndex),
-            new vscode.Position(nextline.lineNumber, nextline.firstNonWhitespaceCharacterIndex)
-        ));
-    }
-}
+export function deactivate() { }
